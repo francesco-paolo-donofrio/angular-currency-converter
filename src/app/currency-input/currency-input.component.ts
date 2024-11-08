feb7f88805be1f3f45ce7d25226f240b7884b113
Frankosviz <geom.francescopaolo@gmail.com>
2024-11-08 21:22:51 +0100

    API call directly in the Currency-input-component + Creating service to handle the API

--- src/app/currency-input/currency-input.component.ts
+++ src/app/currency-input/currency-input.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-currency-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule,],
   templateUrl: './currency-input.component.html',
   styleUrl: './currency-input.component.css'
 })
@@ -15,6 +16,18 @@ export class CurrencyInputComponent {
 
   @Output() currencyChange = new EventEmitter<string>();
   @Output() amountChange = new EventEmitter<number>();
+
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.fetchCurrencies();
+  }
+
+  fetchCurrencies(): void {
+    this.http.get('https://api.frankfurter.app/currencies')
+      .subscribe((data) => {
+        this.currencies = Object.keys(data);});
+  }
 
   onCurrencyChange(event: any) {
     this.currencyChange.emit(event.target.value);
