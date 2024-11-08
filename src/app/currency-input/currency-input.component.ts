f70ea7bc5b686e279fe03c13759a2cb083582cdb
Frankosviz <geom.francescopaolo@gmail.com>
2024-11-08 19:14:08 +0100

    CurrencyInputComponent Created

---
+++ src/app/currency-input/currency-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './currency-input.component.html',
+  styleUrl: './currency-input.component.css'
+})
+export class CurrencyInputComponent {
+  
+}
